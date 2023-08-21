e23b49c063a4119858bf0b660ecc5bd4e968c8ab
Mordy fandio <ordybeni@gmail.com>
2023-08-21 17:21:55 +0100

    using Fetch API

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-const BlogList  = ({blog, title, handleDelete}) => {
+const BlogList  = ({blog, title}) => {
    
 
 
@@ -9,9 +9,8 @@ const BlogList  = ({blog, title, handleDelete}) => {
             {blog.map((blog) =>(
               <div className="blog-preview" key={blog.id}>
                   <h2>{blog.title}</h2>
-                  <p>{blog.body}</p>
                   <p>Written by {blog.author}</p>
-                  <button onClick={()=> handleDelete(blog.id)}>Delete Blog</button>
+                  
               </div>
             ))}
         </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,23 +1,38 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import BlogList from "./BlogList"
 
 const Home = () => {
-    const [blogs,setBlogs]=useState([
-        {title:"My new website", body:"lorem ipsum...",author:"Mario",id:1},
-        {title:"Welcome party", body:"lorem ipsum...",author:"Luigi",id:2}
-    ]);
-    
-    const handleDelete =(id) =>{
-        const newBlog= blogs.filter(blog => blog.id!==id);
-        setBlogs(newBlog);
-    }
+    const [blogs,setBlogs]=useState(null);
+    const [isPending,setIsPending]=useState(true);
+    const [error,setError]=useState(null);
 
-   
+
+   useEffect(()=>{
+      setTimeout(()=>{
+        fetch('http://localhost:8000/blogs')
+      .then(res=>{
+        if(!res.ok){
+          throw Error('could not fetch the data for that resource !');
+        }
+        return res.json();
+      })
+      .then(data =>{
+        setBlogs(data)
+        setIsPending(false)
+        setError(null)
+      })
+      .catch(err=>{
+        setError(err.message)
+        setIsPending(false)
+      })
+      },2000)
+   },[]);
 
     return (
         <div className="home">
-            <BlogList blog={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            
+            {error && <div style={{color:'red',fontSize:18,marginTop:18}}>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blog={blogs} title="All Blogs" />}
         </div>
          
         
